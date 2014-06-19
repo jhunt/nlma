bdaca9db62e8d7d438a06eb4e667cddf62765f51
James Hunt <jhunt@synacor.com>
2014-06-19 09:50:06 -0400

    NLMA v2.6

--- lib/Nagios/Agent.pm
+++ lib/Nagios/Agent.pm
@@ -26,7 +26,7 @@ my %STATE_CODES = (
 my %LOCKS = ();
 my $OOB_PREFIX = "oob";
 
-our $VERSION = '2.5';
+our $VERSION = '2.6';
 
 sub MAX { my ($a, $b) = @_; ($a > $b ? $a : $b); }
 sub MIN { my ($a, $b) = @_; ($a < $b ? $a : $b); }
